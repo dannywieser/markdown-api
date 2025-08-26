347093ec2d6f7f779a805536c339482b7d125d21
Danny Wieser <danny.wieser@gmail.com>
2025-08-26 06:28:57 -0600

    fix: improve SQL statement

--- src/server/interfaces/bear/main.ts
+++ src/server/interfaces/bear/main.ts
@@ -3,7 +3,8 @@ import { loadDatabase } from './database'
 export async function noteByUniqueId(noteUniqueId: string) {
   const db = await loadDatabase()
   const { ZTEXT: note } = await db.get(
-    `SELECT ZTEXT FROM ZSFNOTE where ZUNIQUEIDENTIFIER='${noteUniqueId}'`
+    `SELECT ZTEXT FROM ZSFNOTE where ZUNIQUEIDENTIFIER=?`,
+    [noteUniqueId]
   )
   return { note }
 }
