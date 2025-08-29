536732db270fe0b6e585781727ad3c9c5f52f10d
Danny Wieser <danny.wieser@gmail.com>
2025-08-29 14:20:18 -0600

    feat: add marked and do basic lexing

--- src/server/interfaces/bear/main.ts
+++ src/server/interfaces/bear/main.ts
@@ -1,4 +1,8 @@
-import { MarkdownInit, NoteResponse } from '../interfaces.types'
+import { lexer as markdownLexer } from 'marked'
+
+import { convertDate } from '@/util'
+
+import { MarkdownInit, MarkdownNote } from '../interfaces.types'
 import { backupBearDatabase, loadDatabase } from './database'
 
 export async function init(): Promise<MarkdownInit> {
@@ -10,10 +14,27 @@ export async function init(): Promise<MarkdownInit> {
 export async function noteById(
   noteId: string,
   { db = undefined }: MarkdownInit
-): Promise<NoteResponse | null> {
+): Promise<MarkdownNote | null> {
   if (!db) {
     throw new Error('database not ready')
   }
-  const result = await db.get(`SELECT ZTEXT FROM ZSFNOTE where ZUNIQUEIDENTIFIER=?`, [noteId])
-  return result ? { note: result.ZTEXT } : null
+  const result = await db.get(
+    `SELECT ZTEXT, ZMODIFICATIONDATE, ZCREATIONDATE  FROM ZSFNOTE where ZUNIQUEIDENTIFIER=?`,
+    [noteId]
+  )
+
+  const {
+    ZCREATIONDATE: creationDate,
+    ZMODIFICATIONDATE: modificationDate,
+    ZTEXT: noteText = '',
+  } = result
+  const tokens = markdownLexer(noteText)
+
+  return {
+    created: convertDate(creationDate),
+    id: noteId,
+    modified: convertDate(modificationDate),
+    source: 'bear',
+    tokens,
+  }
 }
