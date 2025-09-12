52b138f52e0fe902d0a69475970dd3bb67e940a7
Danny Wieser <danny.wieser@gmail.com>
2025-09-12 06:53:25 -0600

    tidy things up

--- src/config/loadConfig.ts
+++ src/config/loadConfig.ts
@@ -2,13 +2,11 @@ import { createDir, createFile, expandPath, fileExists, readJSONFile } from '../
 import defaultConfig from './config.default.json'
 import { Config } from './config.types'
 
-const rootDir = '~/.bear-markdown-api'
-
 export async function loadConfig(): Promise<Config> {
-  const configPath = expandPath(`${rootDir}/config.json`)
+  const rootDir = expandPath('~/.bear-markdown-api')
+  const configPath = `${rootDir}/config.json`
   if (!fileExists(configPath)) {
-    const resolvedRootDir = expandPath(rootDir)
-    createDir(resolvedRootDir)
+    createDir(rootDir)
     createFile(configPath, defaultConfig)
     return defaultConfig
   } else {
